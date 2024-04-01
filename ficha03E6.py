1a32c0019e4d61a5c71520059a8f295a0d21500f
Santiago <sg2601710@gmail.com>
2024-04-01 16:18:29 -0300

    Ejercicio 6 ficha 03

---
+++ ficha03E6.py
@@ -0,0 +1,9 @@
+__author__ = "Santiago"
+
+salarioActual = 300
+nombreEmpleado = "Pedro"
+areaFuncional = "Higiene y seguridad"
+nuevoSalario = ((salarioActual*0.08) - (salarioActual*0.025)) + salarioActual
+print("Nombre Empleado:", nombreEmpleado, f"\tNuevo Salario: ${nuevoSalario}")
+print("Área Funcional:", areaFuncional)
+print(f"Salario Actual: ${salarioActual}")

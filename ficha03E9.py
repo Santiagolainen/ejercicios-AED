03dc3a4b9de21bfd2b6b7fd5ca002b4033bc38da
Santiago <sg2601710@gmail.com>
2024-04-01 16:53:08 -0300

    Ejercicio 9 ficha 03 (no lo entedí)

---
+++ ficha03E9.py
@@ -0,0 +1,9 @@
+__author__ = "Santiago"
+
+# No entendí bien el problema asi que voy a hacer lo que entendí
+
+x = int(input("Ingrese el presupuesto apra el sistema: "))
+
+porcentaje = x*0.17
+
+print("Lo máximo que se puede gastar en el proyecto es:", porcentaje)

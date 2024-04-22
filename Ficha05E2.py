40d0c049e3fd80e8792bffdc789b618ed23c698f
Santiago <sg2601710@gmail.com>
2024-04-22 18:18:55 -0300

    Ejercicio 2 ficha 05 más complejo

--- Ficha05E2.py
+++ Ficha05E2.py
@@ -1,5 +1,14 @@
 __author__ = "Santiago"
 
+
+def orden_partidos(partidos):
+    for i in range(len(partidos)):
+        for j in range(len(partidos) - 1 - i):
+            if partidos[j][1] < partidos[j + 1][1]:
+                partidos[j], partidos[j + 1] = partidos[j + 1], partidos[j]
+    return partidos
+
+
 # Ingreso de datos por teclado
 b = 0
 
@@ -19,26 +28,28 @@ por_v2 = round((vot2 * 100) / votos_total, 2)
 por_v3 = round((vot3 * 100) / votos_total, 2)
 
 votPartidos = [[form1, vot1, por_v1], [form2, vot2, por_v2], [form3, vot3, por_v3]]
-votPartidos.sort(key=lambda x: x[1], reverse=True)
+partidos_ordenados = orden_partidos(votPartidos)
 
-primerPartido = votPartidos[0]
-segundoPartido = votPartidos[1]
-
-if primerPartido[2] > 45:
+if partidos_ordenados[0][2] > 45:
     b = 1
-    partidoGanador = primerPartido[0]
-    votosGanador = primerPartido[1]
-    porcentajeGanador = primerPartido[2]
+    partidoGanador = partidos_ordenados[0][0]
+    votosGanador = partidos_ordenados[0][1]
+    porcentajeGanador = partidos_ordenados[0][2]
 else:
-    partido1 = primerPartido[0]
-    votosPartido1 = primerPartido[1]
-    porcentajePartido1 = primerPartido[2]
-    partido2 = segundoPartido[0]
-    votosPartido2 = segundoPartido[1]
-    porcentajePartido2 = segundoPartido[2]
+    partido1 = partidos_ordenados[0][0]
+    votosPartido1 = partidos_ordenados[0][1]
+    porcentajePartido1 = partidos_ordenados[0][2]
+    partido2 = partidos_ordenados[1][0]
+    votosPartido2 = partidos_ordenados[1][1]
+    porcentajePartido2 = partidos_ordenados[1][2]
 
 if b == 1:
-    print(f"El partido {partidoGanador} ganó en primera vuelta con un {porcentajeGanador}% de votos, siendo eso {votosGanador} de votos")
+    print(
+        f"El partido {partidoGanador} ganó en primera vuelta con un {porcentajeGanador}% de votos, siendo eso {votosGanador} de votos")
 else:
-    print(f"Hay segunda vuelta, el primer partido es {partido1} con un {porcentajePartido1}% de votos, siendo eso {votosPartido1} de votos")
-    print(f"El segundo partido es {partido2} con un {porcentajePartido2}% de votos, siendo eso {votosPartido2} de votos")
+    print(
+        f"Hay segunda vuelta, el primer partido es {partido1} con un {porcentajePartido1}% de votos, siendo eso {votosPartido1} de votos")
+    print(
+        f"El segundo partido es {partido2} con un {porcentajePartido2}% de votos, siendo eso {votosPartido2} de votos")
+
+print(orden_partidos(votPartidos))

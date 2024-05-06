b42a3b5c7871037ddfb0c7cca15b8933c7475c47
Santiago <sg2601710@gmail.com>
2024-05-06 18:05:47 -0300

    Ejercicio 3 Ficha 08

---
+++ Ficha08E3.py
@@ -0,0 +1,23 @@
+__author__ = "Santiago"
+
+n = int(input("Ingrese un número (si ingresa 0 se cancela el programa): "))
+i = 0
+num_anterior = nums5 = primer_num = nums_mayor = cont_primer_num = 0
+primer_num = n
+
+if n != 0:
+    i = 1
+
+while n != 0:
+    if n % 10 == 5:
+        nums5 += 1
+    if n == primer_num:
+        cont_primer_num += 1
+    if n > num_anterior:
+        nums_mayor += 1
+    num_anterior = n
+    n = int(input("Ingrese un número (si ingresa 0 se cancela el programa): "))
+
+print(f"\nLa cantidad de números que terminan en 5 son {nums5}\n")
+print(f"La cantidad de veces que aparece el primer número son {cont_primer_num - 1}\n")
+print(f"La cantidad de números que son mayores a su antecesor son {nums_mayor - 1}")

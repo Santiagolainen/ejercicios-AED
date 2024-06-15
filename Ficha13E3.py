481b234e9e05b35a390b41b5b1283b59446d7ff1
Santiago <sg2601710@gmail.com>
2024-06-15 11:55:54 -0300

    Ejercicio 3 Ficha 13

--- Ficha13E3.py
+++ Ficha13E3.py
@@ -23,8 +23,23 @@ def inicio_palabra(car_ant, car):
         return True
     return False
 
+def menu():
+    print('Menu de opciones:')
+    print('Elija una opción\n')
+    print('1 - Promedio general de letras por palabra del texto')
+    print('2 - Cantidad de palabras terminadas en vocal.')
+    print('3 - Orden de la palabra más larga del texto.')
+    print('4 - Cantidad de palabras que contengan al menos una vez la sílaba l+vocal (es decir la, le, li, lo o lu).')
+    print('5 - Cantidad de palabras que incluyan algún dígito en la primera mitad.')
+    print('6 - Salir')
+    op = int(input())
+
+    return op
 
 def main():
+
+    print('Palabras con expresión "l+vocal"')
+    print('='*80)
     texto = get_text()
     
     cont_pal_digitos = letras_palabras = posicion_digito = cont_letras = cont_palabras = cont_pal_fin_vocal = long_pal = pal_max = orden_pal_max = cont_pal_lvocal = 0
@@ -73,11 +88,28 @@ def main():
     
     promedio = prom(cont_letras,cont_palabras)
 
-    print(promedio)
-    print(cont_pal_fin_vocal)
-    print(orden_pal_max)
-    print(cont_pal_lvocal)
-    print(cont_pal_digitos)
+    op = menu()
+
+    while op != 6:
+        if op == 1:
+            print(f'\nEl promedio general de letras por palabra del texto es de {promedio} letras por palabra\n\n')
+            op = menu()
+        elif op == 2:
+            print(f'\nLa cantidad de palabras terminadas en vocal son {cont_pal_fin_vocal} palabras\n\n')
+            op = menu()
+        elif op == 3:
+            print(f'\nEl orden de la palabra más larga del texto es {orden_pal_max}\n\n')
+            op = menu()
+        elif op == 4:
+            print(f'\nLa cantidad de palabras que contienen al menos una vez la silaba "l+vocal" son {cont_pal_lvocal} palabras\n\n')
+            op = menu()
+        elif op == 5:
+            print(f'\nLa cantidad de palabras que incluyen algún dígito en la primera mitad son {cont_pal_digitos} palabras\n\n')
+            op = menu()
+        else:
+            print('\nComando no válido\n\n')
+            op = menu()
+    print('\nFinalizando programa...')
 
 
 
